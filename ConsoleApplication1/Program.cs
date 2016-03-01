22709f3a6889e06c3e17e99025a9a09e86deffde
Bastrakov Sergey <sergei.bastrakov@gmail.com>
2016-03-02 02:26:22 +0300

    Update common solution

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using HardwareSignalsLibrary.IO;
+using HardwareSignalsLibrary.IO.Hardware;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IHardwareSignals signals = new EmptyHardwareSignals(2, 2, 2, 2, 2))
+            {
+                signals.Start();
+                HardwareInputOutput hardwareInputOutput = new HardwareInputOutput(new HardwareInputConfiguration {Signals = signals});
+
+                IInputOutputSystem inputOutputSystem = new InputOutputSystem
+                {
+                    In = hardwareInputOutput,
+                    Out = hardwareInputOutput
+                };
+
+                IInputReader inputReader = inputOutputSystem.In;
+                IOutputWriter outputWriter = inputOutputSystem.Out;
+
+                float acceleration = inputReader.ReadAnalogInput("Acceleration");
+
+                float speed = acceleration;
+
+                outputWriter.WriteAnalogOuput("Speed", speed);
+            }
+        }
+    }
+}
